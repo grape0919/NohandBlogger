2b0682d33cd46d6c7b0189fd358ac924da751ad1
ghdry2563 <ghdry2563@gmail.com>
2020-11-09 03:07:10 +0900

    upload

---
+++ app.py
@@ -0,0 +1,26 @@
+from __future__ import with_statement
+
+from flask import Flask, request, g, redirect, url_for, \
+     abort, render_template, flash
+from flask_bootstrap import Bootstrap
+
+import webbrowser
+
+DEBUG=True
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+Bootstrap(app)
+
+@app.route("/")
+def root():
+     return render_template("childExample.html")
+
+
+if __name__ == '__main__':
+     
+     url = 'http://localhost:5000'
+     webbrowser.open(url)
+     
+     app.run(host='0.0.0.0')
